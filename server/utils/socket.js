d650e0ab172b05b0a6a7b171f568f8be7facb38e
Abhishek <abhishekparmar162@gmail.com>
2025-07-29 15:31:17 +0530

    cors updated socket

--- server/utils/socket.js
+++ server/utils/socket.js
@@ -6,7 +6,10 @@ const onlineUsers = new Map();
 export const setupSocket = (server, app) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: [
+        "http://localhost:3000",
+        "https://mern-chat-app-bice.vercel.app",
+      ],
       methods: ["GET", "POST"],
       credentials: true,
     },
